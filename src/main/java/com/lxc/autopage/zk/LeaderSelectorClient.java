bce1b62c6c97c3d6c0cea8ca9465772a73d2e908
chenluxing <chenluxing0918@163.com>
2016-10-28 17:56:34 +0800

    添加zookeeper的Leader

--- src/main/java/com/lxc/autopage/zk/LeaderSelectorClient.java
+++ src/main/java/com/lxc/autopage/zk/LeaderSelectorClient.java
@@ -11,6 +11,7 @@ import java.io.IOException;
  * 节点Leader选举
  * Created by chenlx
  * on 2016/10/28.
+ * http://www.open-open.com/lib/view/open1434353619567.html
  */
 public abstract class LeaderSelectorClient extends LeaderSelectorListenerAdapter implements Closeable {
 
